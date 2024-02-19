156c02cb41df8c75c096663ea81aacc06c25b432
Ronald Trang <rtrang@uwaterloo.ca>
2024-02-18 19:59:22 -0500

    Modified filePlotter.py and created graphs

--- filePlotter.py
+++ filePlotter.py
@@ -11,16 +11,50 @@ def plot_errors(filename):
     time_list=[]
     first_stamp=values[0][-1]
     
+    motions = ['circle', 'spiral', 'line']
+    topicType = ['imu', 'odom', 'laser']
+    motionType = ''
+    dataType = ''
+
+    for word in topicType:
+        for move in motions:
+            if word in filename:
+                dataType = word
+            if move in filename:
+                motionType = move
+
     for val in values:
         time_list.append(val[-1] - first_stamp)
 
-    for i in range(0, len(headers) - 1):
-        plt.plot(time_list, [lin[i] for lin in values], label= headers[i]+ " linear")
+    if dataType == 'odom':
+        for i in range(0, len(headers) - 1):
+            if headers[i] == 'x':
+                headers[i] = 'X-Position [m]'
+            elif headers[i] =='y':
+                headers[i] = 'Y-Position [m]'
+            elif headers[i] == 'th':
+                headers[i] = 'Angle [radians]'
+
+            plt.plot(time_list, [lin[i] for lin in values], label= headers[i])
+    elif dataType == 'imu':
+        for i in range(0, len(headers) - 1):
+            if headers[i] == 'acc_x':
+                headers[i] = 'X-Acceleration [m/s^2]'
+            elif headers[i] =='acc_y':
+                headers[i] = 'Y-Acceleration [m/s^2]'
+            elif headers[i] == 'angular_z':
+                headers[i] = 'Z-Angular_Vel [radians/s]'
+
+            plt.plot(time_list, [lin[i] for lin in values], label= headers[i])
     
     #plt.plot([lin[0] for lin in values], [lin[1] for lin in values])
     plt.legend()
     plt.grid()
+    plt.xlabel('Time [nanoseconds]')
+    plt.ylabel('Magnitude')
+    plt.title(dataType + ' Data for ' + motionType + ' path')
     plt.show()
+    plt.savefig('plots/' + dataType + motionType + '.jpg')
     
 import argparse
 
